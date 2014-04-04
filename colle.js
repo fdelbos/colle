a26b3f5671e6eff4d113e7f11153493c069fccbf
Frederic Delbos <fred.delbos@gmail.com>
2014-04-04 02:14:18 +0200

    pass jsint! bump version

--- colle.js
+++ colle.js
@@ -1,100 +1,129 @@
+/* jslint node: true */
 "use strict";
 
-var _ = require("lodash")
+var _ = require("lodash");
 
-var make = function() {
-    var _factories = {}
-    var _treated = {}
-    var _instances = {}
+var make = function () {
+    var _factories = {},
+        _treated = {},
+        _instances = {},
+        colleError, // throw when something go wrong
+        call, // call start construction callback with or without err
+        set, // set a dependency
+        get, // get a dependency (after start)
+        genDoNext, // call buildFactory with the next dependency
+        buildFactory, // build the factory
+        complete,// complete start function, by cleaning temporary stuffs and call cb with success
+        start; // take care of building everything and execute a call back for success or error
 
-    var colleError = function(msg) {
+    colleError = function (msg) {
         throw "!!! COLLE ERROR !!!\n ->" + msg + "\n";
-    }
+    };
 
-    var call = function(cb, err) {
-        if (_.isFunction(cb))
+    call = function (cb, err) {
+        if (_.isFunction(cb)) {
             cb(err);
-    }
+        }
+    };
 
-    var set = function(name, dependencies, builder) {
-        if (!_.isString(name))
+    set = function (name, dependencies, builder) {
+        if (!_.isString(name)) {
             colleError("'colle.set' name parameter must be a string.");
-        if (!_.isArray(dependencies))
+        }
+        if (!_.isArray(dependencies)) {
             colleError("'colle.set' dependencies parameter must be an array.");
-        if (!_.isFunction(builder))
+        }
+        if (!_.isFunction(builder)) {
             colleError("'colle.set' builder parameter must be a function.");
+        }
         _factories[name] = {
             dependencies: dependencies,
-            builder: builder,
-        }
-    }
+            builder: builder
+        };
+    };
 
-    var get = function(name) {
+    get = function (name) {
         return _instances[name];
-    }
+    };
 
-    var buildFactory = function(name, cb) {
-        if (_.has(_instances, name))
-            call(cb, null);
-        if (!_.has(_factories, name))
-			colleError("unknow factory: '" + name + "'");
-        _treated[name] = true;
-        var params = []
-        var dependencies = _factories[name].dependencies
-        for (var d in dependencies) {
-            var depName = dependencies[d];
-            if(_.has(_treated, depName))
-                colleError("cyclic dependency detected: '" + name + "'");
-            if(!_.has(_instances, depName)) {
-                return buildFactory(depName, function(err) {
-					if (err)
-						return call(cb, err);
-					else
-						return buildFactory(name, cb);
-					});
+    genDoNext = function (name, endCb) {
+        return function (err) {
+            if (err) {
+                return call(endCb, err);
             }
-            params.push(_instances[depName]);
-        }
+            return buildFactory(name, endCb);
+        };
+    };
 
-        var instance = _factories[name].builder.apply(null, params)
-        _instances[name] = instance
+    complete = function (name, startCb) {
+        delete _factories[name];
+        delete _treated[name];
+        call(startCb, null);
+    };
 
-        var complete = function() {
-            delete _factories[name];
-            delete _treated[name];
+    buildFactory = function (name, cb) {
+        if (_.has(_instances, name)) {
             call(cb, null);
         }
+        if (!_.has(_factories, name)) {
+            colleError("unknow factory: '" + name + "'");
+        }
+        _treated[name] = true;
 
-        if (_.isFunction(instance._init)) {
-            instance._init(function(err) { 
-				if (err) 
-					return call(cb, err)
-				else
-					return complete(); 
-			});
+        var params = [],
+            dependencies = _factories[name].dependencies,
+            i;
+        for (i = 0; i < dependencies.length; i += 1) {
+            if (_.has(_treated, dependencies[i])) {
+                colleError("cyclic dependency detected: '" + name + "'");
+            }
+            if (!_.has(_instances, dependencies[i])) {
+                return buildFactory(dependencies[i], genDoNext(name, cb));
+            }
+            params.push(_instances[dependencies[i]]);
+        }
+
+        _instances[name] = _factories[name].builder.apply(null, params);
+
+        if (_.isFunction(_instances[name]._init)) {
+            _instances[name]._init(function (err) {
+                if (err) {
+                    return call(cb, err);
+                }
+                return complete(name, cb);
+            });
             return;
         }
-        complete();
-    }
+        complete(name, cb);
+    };
 
-    var start = function(cb) {
-        if (!_.isFunction(cb))
-            colleError("'colle.start' callback is not a function, don't know how to start.")
-        if (_.isEmpty(_factories))
-            call(cb, null);
-        for(var k in _factories) {
-            return buildFactory(k, function(err) {
-				if (err)
-					return call(cb, err);
-				return start(cb);});
+    start = function (cb) {
+        if (!_.isFunction(cb)) {
+            colleError("'colle.start' callback is not a function, don't know how to start.");
         }
-    }
+        if (_.isEmpty(_factories)) {
+            call(cb, null);
+        }
+
+        var startNext = function (err) {
+                if (err) {
+                    return call(cb, err);
+                }
+                return start(cb);
+            },
+            name;
+        for (name in _factories) {
+            if (_factories.hasOwnProperty(name)) {
+                return buildFactory(name, startNext);
+            }
+        }
+    };
 
     return {
         start: start,
         get: get,
         set: set
-    }
+    };
 }
 
 exports.make = make;
